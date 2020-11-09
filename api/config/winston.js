4623face22003baa77317af6a19d6bff13f179a9
Aparna Ashok <ashok.a@husky.neu.edu>
2020-11-09 15:43:10 -0800

    Configuration changes

--- api/config/winston.js
+++ api/config/winston.js
@@ -1,14 +1,4 @@
 var winston = require('winston');
-
-const logger = new winston.Logger({
-    transports: [
-        new (winston.transports.Console)({
-            timestamp: true,
-            colorize: true,
-        })
-    ]
-});
-
 var options = {
     file: {
         level: 'info',
@@ -41,13 +31,11 @@ var logger = new winston.createLogger({
         new winston.transports.File(options.file),
         new winston.transports.Console(options.console)
     ],
-    exitOnError: false, // do not exit on handled exceptions
+    exitOnError: false,
 });
 
-// create a stream object with a 'write' function that will be used by `morgan`
 logger.stream = {
     write: function(message, encoding) {
-        // use the 'info' log level so the output will be picked up by both transports (file and console)
         logger.info(message);
     },
 };
