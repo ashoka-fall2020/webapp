9fb1e8f5ae45f9add4d2f1a8c425394dc53f34f4
Aparna Ashok <ashok.a@husky.neu.edu>
2020-11-10 12:10:59 -0800

    app start command change

--- codedeploy/app_start.sh
+++ codedeploy/app_start.sh
@@ -8,11 +8,7 @@ sudo npm i pm2 -g
 cd webapp
 sudo cp /home/ubuntu/.env ./.env
 echo "starting cloud watch"
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
-    -a fetch-config \
-    -m ec2 \
-    -c file:/home/ubuntu/webapp/cloudwatch-config.json \
-    -s
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/webapp/cloudwatch-config.json -s
 echo "starting webapp"
 sudo pm2 start server.js
 echo "webapp running.."
