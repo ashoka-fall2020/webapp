e33c25ccf3c2a88303086c985087c84e24428aad
Aparna Ashok <ashok.a@husky.neu.edu>
2020-12-09 21:51:05 -0800

    package removed

--- api/config/dbconfig.js
+++ api/config/dbconfig.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
+require('mysql2');
 module.exports = {
     HOST: process.env.DATABASE_HOST_NAME,
     USER: process.env.DATABASE_USER_NAME,
     PASSWORD: process.env.DATABASE_PASSWORD,
     DB: process.env.DATABASE_NAME,
     PORT: process.env.DATABASE_PORT,
-    dialect: 'mysql',
+    dialect: 'mysql' ,
+    dialectOptions: {
+        ssl: 'Amazon RDS'
+    },
     pool: {
         max: 5,
         min: 0,
